4e1489861e016e3b30cac04eb8e8d1c0d7a65609
Pauli Jokela <pauli.jokela@didstopia.com>
2017-11-05 10:54:34 +0200

    Sanity checks for GitHub tags and deployments

---
+++ .scripts/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git config --global user.email "builds@travis-ci.com"
+git config --global user.name "Travis CI"
+
+export CLOVER_PKG_NAME=$(echo -n $HOME/src/edk2/Clover/CloverPackage/sym/Clover_*.pkg)
+export CLOVER_PKG_NAME=$(basename ${CLOVER_PKG_NAME})
+export CLOVER_PKG_NAME=$(echo -n ${CLOVER_PKG_NAME/.pkg/})
+export GIT_TAG=$(echo -n ${CLOVER_PKG_NAME/Clover_/})
+
+if [[ `git tag -l $GIT_TAG` == $GIT_TAG ]]; then
+    echo "Tag already exists, skipping deployment.."
+else
+    git tag $GIT_TAG -a -m ''
+    git push -q https://$GITHUB_OAUTH_TOKEN@github.com/Dids/clover-builder --tags
+fi
