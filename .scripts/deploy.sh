54f8c20dbd278140d9afc3f820251b915f48f69a
Pauli Jokela <pauli.jokela@didstopia.com>
2017-11-05 14:06:07 +0200

    Fixed deployment errors when tag hasn't changed

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -2,17 +2,20 @@
 
 set -x
 
+# Setup git user
 git config --global user.email "builds@travis-ci.com"
 git config --global user.name "Travis CI"
 
+# Figure out the tag name
 export CLOVER_PKG_NAME=$(echo -n $HOME/src/edk2/Clover/CloverPackage/sym/Clover_*.pkg)
 export CLOVER_PKG_NAME=$(basename ${CLOVER_PKG_NAME})
 export CLOVER_PKG_NAME=$(echo -n ${CLOVER_PKG_NAME/.pkg/})
 export GIT_TAG=$(echo -n ${CLOVER_PKG_NAME/Clover_/})
 
+# Update tags
 git fetch --tags
-REMOTE_TAG=$(git ls-remote --tags 2>/dev/null | grep $GIT_TAG 1>/dev/null)
-git tag -l
+
+# Compare current tag against the built tag
 CURRENT_TAG=$(git tag -l $GIT_TAG)
 if [[ "$CURRENT_TAG" == "$GIT_TAG" ]]; then
     echo "Tag already exists, skipping deployment.."
