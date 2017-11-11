ca76b50cc31b75c7221a1d18878ab7ed26cee835
Travis CI <builds@travis-ci.com>
2017-11-11 13:32:43 +0200

    Fixing invalid deployment tag issue

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -10,6 +10,13 @@ export CLOVER_PKG_NAME=$(basename ${CLOVER_PKG_NAME})
 export CLOVER_PKG_NAME=$(echo -n ${CLOVER_PKG_NAME/.pkg/})
 export GIT_TAG=$(echo -n ${CLOVER_PKG_NAME/Clover_/})
 
+# Verify that we have a valid tag
+if [[ -z "${GIT_TAG// }" || "${GIT_TAG// }" != v* ]]; then
+	echo "Invalid tag '$GIT_TAG', aborting deployment.."
+	rm -fr "$HOME/src/edk2/Clover/CloverPackage/sym/Clover_*.pkg"
+	exit 0
+fi
+
 # Update tags
 git fetch --tags
 
@@ -18,6 +25,7 @@ CURRENT_TAG=$(git tag -l $GIT_TAG)
 if [[ "$CURRENT_TAG" == "$GIT_TAG" ]]; then
     echo "Tag already exists, skipping deployment.."
 else
+	echo "Pushing tag: $GIT_TAG"
     git tag $GIT_TAG -a -m ''
     git push -q https://$GITHUB_OAUTH_TOKEN@github.com/Dids/clover-builder --tags
 fi
